04ba28ebbd14b20deebd3b2e7132f5a0244b15ee
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-05-24 12:18:18 +0800

    action service listen to msgs and create user action log setup done

--- be-service/action/src/main/java/com/example/action/kafka/listener/UserActionListener.java
+++ be-service/action/src/main/java/com/example/action/kafka/listener/UserActionListener.java
@@ -1,13 +1,36 @@
 package com.example.action.kafka.listener;
 
+import com.example.action.DTO.UserActionDTO;
+import com.example.action.common.enums.ActionOnItem;
+import com.example.action.service.ActionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserActionListener {
+
+    @Autowired
+    private ActionService actionService;
+
+    public UserActionListener(ActionService actionService){
+        this.actionService = actionService;
+    }
+
     @KafkaListener(topics = "user-action")
     public void createActionListener(String kafkaMsg) {
-        System.out.println("test");
-        System.out.println(kafkaMsg);
+        try {
+            String[] parts = kafkaMsg.split(";");
+            if (parts.length != 3){
+                throw new IllegalArgumentException();
+            }
+            UserActionDTO userActionDTO = new UserActionDTO();
+            userActionDTO.setActionOnItem(ActionOnItem.valueOf(parts[0]));
+            userActionDTO.setItemId(Integer.parseInt(parts[1]));
+            userActionDTO.setUserUuid(parts[2]);
+            actionService.create(userActionDTO);
+        } catch (Exception e){
+            System.out.println("Invalid kafka msg: " + kafkaMsg);
+        }
     }
 }
